05249e526c5c94f04966872df8f7f27229baf174
mohinik7 <149883645+mohinik7@users.noreply.github.com>
2024-08-31 22:14:27 +0530

    Add files via upload

---
+++ data/script.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.interaction span').forEach((element) => {
+        element.addEventListener('click', () => {
+            alert('Feature coming soon!');
+        });
+    });
+});
